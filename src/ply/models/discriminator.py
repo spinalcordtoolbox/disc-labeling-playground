47d623fea3a972cd58ee33f591bfa45727c05dee
Nathan Molinier <nathan.molinier@gmail.com>
2024-03-12 13:35:51 -0400

    improve clarity

--- src/ply/models/discriminator.py
+++ src/ply/models/discriminator.py
@@ -11,11 +11,11 @@ class Discriminator(nn.Module):
     def __init__ (self, feature_size=32, kernel_size=[4,4,4]):
         super(Discriminator, self).__init__()
                 
-        self.disc_conv1 = torch.nn.Conv3d(1, feature_size, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv2 = torch.nn.Conv3d(feature_size, feature_size*2, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv3 = torch.nn.Conv3d(feature_size*2, feature_size*4, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv4 = torch.nn.Conv3d(feature_size*4, feature_size*8, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv5 = torch.nn.Conv3d(feature_size*8, 1, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv1 = nn.Conv3d(1, feature_size, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv2 = nn.Conv3d(feature_size, feature_size*2, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv3 = nn.Conv3d(feature_size*2, feature_size*4, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv4 = nn.Conv3d(feature_size*4, feature_size*8, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv5 = nn.Conv3d(feature_size*8, 1, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         
         self.disc_bn1 = nn.BatchNorm3d(32)
         self.disc_bn2 = nn.BatchNorm3d(64)
