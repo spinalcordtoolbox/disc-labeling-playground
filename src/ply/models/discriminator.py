96639dbc6db54eb5d46c0a3c067414d99ccc2239
Nathan Molinier <nathan.molinier@gmail.com>
2024-03-14 10:32:32 -0400

    add variables disc

--- src/ply/models/discriminator.py
+++ src/ply/models/discriminator.py
@@ -8,19 +8,19 @@ import torch.nn.functional as F
 
                       
 class Discriminator(nn.Module):
-    def __init__ (self, feature_size=32, kernel_size=[4,4,4]):
+    def __init__ (self, in_channels=1, feature_size=32, kernel_size=[4,4,4]):
         super(Discriminator, self).__init__()
                 
-        self.disc_conv1 = nn.Conv3d(1, feature_size, kernel_size=kernel_size, stride=[2,2,2], padding=1)
+        self.disc_conv1 = nn.Conv3d(in_channels, feature_size, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         self.disc_conv2 = nn.Conv3d(feature_size, feature_size*2, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         self.disc_conv3 = nn.Conv3d(feature_size*2, feature_size*4, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         self.disc_conv4 = nn.Conv3d(feature_size*4, feature_size*8, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         self.disc_conv5 = nn.Conv3d(feature_size*8, 1, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         
-        self.disc_bn1 = nn.BatchNorm3d(32)
-        self.disc_bn2 = nn.BatchNorm3d(64)
-        self.disc_bn3 = nn.BatchNorm3d(128)
-        self.disc_bn4 = nn.BatchNorm3d(256)
+        self.disc_bn1 = nn.BatchNorm3d(feature_size)
+        self.disc_bn2 = nn.BatchNorm3d(feature_size*2)
+        self.disc_bn3 = nn.BatchNorm3d(feature_size*4)
+        self.disc_bn4 = nn.BatchNorm3d(feature_size*8)
         
         self.LRelu = nn.LeakyReLU(0.2, True)
     
