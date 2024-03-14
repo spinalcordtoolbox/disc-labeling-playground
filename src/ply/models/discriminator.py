0269dd9cd73cc962fe2ab17e9282574f440be790
Nathan Molinier <nathan.molinier@gmail.com>
2024-03-14 11:40:54 -0400

    Rewrite layers

--- src/ply/models/discriminator.py
+++ src/ply/models/discriminator.py
@@ -1,38 +1,62 @@
 '''
-Based on https://github.com/cihanongun/3D-CGAN/
+Based on https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/GANs/Pix2Pix/discriminator_model.py
 '''
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=[4,4,4], stride=[2,2,2], padding=1):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(
+                in_channels, out_channels, kernel_size, stride, padding, bias=False, padding_mode="reflect"
+            ),
+            nn.BatchNorm3d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
                       
 class Discriminator(nn.Module):
-    def __init__ (self, in_channels=1, feature_size=32, kernel_size=[4,4,4]):
+    def __init__ (self, in_channels=1, features=[16, 32, 64, 128], kernel_size=[4,4,4]):
         super(Discriminator, self).__init__()
-                
-        self.disc_conv1 = nn.Conv3d(in_channels, feature_size, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv2 = nn.Conv3d(feature_size, feature_size*2, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv3 = nn.Conv3d(feature_size*2, feature_size*4, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv4 = nn.Conv3d(feature_size*4, feature_size*8, kernel_size=kernel_size, stride=[2,2,2], padding=1)
-        self.disc_conv5 = nn.Conv3d(feature_size*8, 1, kernel_size=kernel_size, stride=[2,2,2], padding=1)
         
-        self.disc_bn1 = nn.BatchNorm3d(feature_size)
-        self.disc_bn2 = nn.BatchNorm3d(feature_size*2)
-        self.disc_bn3 = nn.BatchNorm3d(feature_size*4)
-        self.disc_bn4 = nn.BatchNorm3d(feature_size*8)
-        
-        self.LRelu = nn.LeakyReLU(0.2, True)
+        self.initial = nn.Sequential(
+            nn.Conv3d(
+                in_channels * 2,
+                features[0],
+                kernel_size=kernel_size,
+                stride=[2,2,2],
+                padding=1,
+                padding_mode="reflect",
+            ),
+            nn.LeakyReLU(0.2),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                CNNBlock(in_channels, feature, kernel_size=kernel_size, stride=[1,1,1] if feature == features[-1] else [2,2,2])
+                )
+            in_channels = feature
+
+        layers.append(
+            nn.Conv3d(in_channels, 1, kernel_size=kernel_size, stride=[1,1,1], padding=1, padding_mode="reflect")
+            )
+
+        self.model = nn.Sequential(*layers)
     
     def forward(self, x, y):
         
         x = torch.cat([x, y], dim=1)
-        
-        x = self.LRelu(self.disc_bn1(self.disc_conv1(x)))
-        x = self.LRelu(self.disc_bn2(self.disc_conv2(x)))
-        x = self.LRelu(self.disc_bn3(self.disc_conv3(x)))
-        x = self.LRelu(self.disc_bn4(self.disc_conv4(x)))
-        x = self.disc_conv5(x)
-        x = torch.sigmoid(x)
+        x = self.initial(x)
+        x = self.model(x)
+        #x = torch.sigmoid(x)
         
         return x.squeeze()
