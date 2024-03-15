db5fde8a33a9279cd3c7793d2c9169b7e8d2e7c0
Nathan Molinier <nathan.molinier@gmail.com>
2024-03-15 11:51:30 -0400

    add new function do extract image for training

--- src/ply/utils/plot.py
+++ src/ply/utils/plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 def save_bar(names, values, output_path, x_axis, y_axis):
@@ -38,3 +39,43 @@ def plot_discs_distribution(discs_labels_list, out_path):
     names, values = list(plot_discs.keys()), list(plot_discs.values())
     # Plot distribution
     save_bar(names, values, out_path, x_axis='Discs number', y_axis='Quantity')
+
+
+def get_validation_image(in_img, target_img, pred_img):
+    in_img = in_img.data.cpu().numpy()
+    target_img = target_img.data.cpu().numpy()
+    pred_img = pred_img.data.cpu().numpy()
+    in_all = []
+    target_all = []
+    pred_all = []
+    for num_batch in range(in_img.shape[0]):
+        # Load 3D numpy array
+        x = in_img[num_batch, 0]
+        y = target_img[num_batch, 0]
+        y_pred = pred_img[num_batch, 0]
+        shape = x.shape
+
+        # Extract middle slice
+        x = x[shape[0]//2,:,:]
+        y = y[shape[0]//2,:,:]
+        y_pred = y_pred[shape[0]//2,:,:]
+
+        # Normalize intensity
+        x = x/np.max(x)*255
+        y = y/np.max(y)*255
+        y_pred = y_pred/np.max(y_pred)*255
+
+        # Regroup batch
+        in_all.append(x)
+        target_all.append(y)
+        pred_all.append(y_pred)
+    
+    # Regroup batch into 1 array
+    in_line_arr = np.concatenate(np.array(in_all), axis=1)
+    target_line_arr = np.concatenate(np.array(target_all), axis=1)
+    pred_line_arr = np.concatenate(np.array(pred_all), axis=1)
+
+    # Regroup image/target/pred into 1 array
+    img_result = np.concatenate((in_line_arr, target_line_arr, pred_line_arr), axis=0)
+    
+    return img_result, target_line_arr, pred_line_arr
