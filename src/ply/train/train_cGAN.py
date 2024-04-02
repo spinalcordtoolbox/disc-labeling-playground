0df57059bae4348e3691ea251156b9e803bc1671
Nathan Molinier <nathan.molinier@gmail.com>
2024-04-02 16:36:16 -0400

    update comments

--- src/ply/train/train_cGAN.py
+++ src/ply/train/train_cGAN.py
@@ -46,13 +46,13 @@ def get_parser():
     parser.add_argument('--batch-size', type=int, default=3, help='Training batch size (default=3).')
     parser.add_argument('--nb-epochs', type=int, default=300, help='Number of training epochs (default=300).')
     parser.add_argument('--start-epoch', type=int, default=0, help='Starting epoch (default=0).')
-    parser.add_argument('--crop-size', type=tuple_type, default=(64, 320, 192), help='Training crop size in RSP orientation(default=(64, 256, 192)).')
+    parser.add_argument('--crop-size', type=tuple_type, default=(64, 320, 192), help='Training crop size in RSP orientation(default=(64, 320, 192)).')
     parser.add_argument('--channels', type=tuple_type, default=(16, 32, 64, 128, 256), help='Channels if attunet selected')
     parser.add_argument('--pixdim', type=tuple_type, default=(0.8, 0.8, 0.8), help='Training resolution in RSP orientation (default=(0.8, 0.8, 0.8)).')
     parser.add_argument('--alpha', type=int, default=100, help='L1 loss multiplier (default=100).')
-    parser.add_argument('--g-lr', default=2.5e-4, type=float, metavar='LR', help='Initial learning rate of the generator (default=2.5e-5)')
-    parser.add_argument('--d-lr', default=2.5e-5, type=float, metavar='LR', help='Initial learning rate of the discriminator (default=2.5e-6)')
-    parser.add_argument('--schedule', type=tuple_type, default=(0.50, 0.75), help='Decrease learning rate at these steps: fractions of the maximum number of epochs. (default=(0.75, 0.85))')
+    parser.add_argument('--g-lr', default=2.5e-4, type=float, metavar='LR', help='Initial learning rate of the generator (default=2.5e-4)')
+    parser.add_argument('--d-lr', default=2.5e-5, type=float, metavar='LR', help='Initial learning rate of the discriminator (default=2.5e-5)')
+    parser.add_argument('--schedule', type=tuple_type, default=(0.50, 0.75), help='Decrease learning rate at these steps: fractions of the maximum number of epochs. (default=(0.5, 0.75))')
     parser.add_argument('--weight-folder', type=str, default=os.path.abspath('src/ply/weights/3D-CGAN'),
                         help='Folder where the cGAN weights will be stored and loaded. Will be created if does not exist. (default="src/ply/weights/3DGAN")')
     return parser
