b9ba04527a71e6789f9f8ba218a93537292e36ef
Nathan Molinier <nathan.molinier@gmail.com>
2024-04-09 14:39:02 -0400

    add violin plot

--- src/ply/utils/plot.py
+++ src/ply/utils/plot.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 def save_bar(names, values, output_path, x_axis, y_axis):
     '''
@@ -23,6 +25,40 @@ def save_bar(names, values, output_path, x_axis, y_axis):
     plt.xticks(names)
     plt.title("Discs distribution")
     plt.savefig(output_path)
+
+def save_violin(data, outpath, x_names=['data'], x_axis='data', y_axis='values'):
+    '''
+    Create violin plot
+    '''
+    if isinstance(data[0],list):
+        if (len(data) != len(x_names)):
+            raise ValueError('Missing x names')
+    else:
+        if len(x_names) != 1:
+            raise ValueError('Only one 1 name should be used for the x axis')
+        else:
+            data = [data]
+    
+    plot_dict = {'data' : [], 'values' : []}
+    for name, d in zip(x_names, data):
+        plot_dict['data'] += [name]*len(d)
+        plot_dict['values'] += d
+    
+    plot_df = pd.DataFrame(data=plot_dict)
+    sns.set(style="darkgrid")
+
+    # Make the plot 
+    plt.figure()
+    sns.violinplot(x="data", y="values", data=plot_df, width=1)
+    plt.title(f'{y_axis} violin plot')
+    plt.xlabel(x_axis, fontsize = 20)
+    plt.ylabel(y_axis, fontsize = 20)
+    plt.title(y_axis, fontsize = 25)
+    
+    # Save plot
+    plt.savefig(outpath)
+
+
 
 
 def plot_discs_distribution(discs_labels_list, out_path):
