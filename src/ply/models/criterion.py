c9fde8da89438fa6264d5cbe2d71ad0f1695a1e5
Nathan Molinier <nathan.molinier@gmail.com>
2024-05-07 15:50:14 -0400

    add gradient penalty function

--- src/ply/models/criterion.py
+++ src/ply/models/criterion.py
@@ -27,3 +27,28 @@ def hinge_d_loss(logits_real, logits_fake):
     loss_fake = torch.mean(F.relu(1. + logits_fake))
     d_loss = 0.5 * (loss_real + loss_fake)
     return d_loss
+
+
+def gradient_penalty(critic, x, y, y_fake, device="cpu"):
+    '''
+    Based on https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/GANs/4.%20WGAN-GP/utils.py
+    '''
+    BATCH_SIZE, C, W, H, D = y.shape
+    alpha = torch.rand((BATCH_SIZE, 1, 1, 1, 1)).repeat(1, C, W, H, D).to(device)
+    interpolated_images = y * alpha + y_fake * (1 - alpha)
+
+    # Calculate critic scores
+    mixed_scores = critic(x, interpolated_images)
+
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
