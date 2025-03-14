fb1a5ab623129fbbd81c9314f44baa3aad354101
Nathan Molinier <nathan.molinier@gmail.com>
2025-03-14 16:05:50 -0400

    update cache_rate to 0.5 and num_workers

--- src/ply/train/train_segmentation.py
+++ src/ply/train/train_segmentation.py
@@ -168,14 +168,14 @@ def main():
     train_ds = CacheDataset(
                             data=train_list,
                             transform=train_transforms,
-                            cache_rate=0.25,
-                            num_workers=5,
+                            cache_rate=0.5,
+                            num_workers=None,
                             )
     val_ds = CacheDataset(
                         data=val_list,
                         transform=val_transforms,
-                        cache_rate=0.25,
-                        num_workers=5,
+                        cache_rate=0.5,
+                        num_workers=None,
                         )
 
     # Define train and val DataLoader
