1741ab999a3fd3e5f9252446c88baa8dc4d5e1b9
Nathan Molinier <nathan.molinier@gmail.com>
2025-03-18 14:04:40 -0400

    reduce cache rate

--- src/ply/train/train_segmentation.py
+++ src/ply/train/train_segmentation.py
@@ -166,12 +166,12 @@ def main():
     train_ds = CacheDataset(
                             data=train_list,
                             transform=train_transforms,
-                            cache_rate=1,
+                            cache_rate=0.1,
                             )
     val_ds = CacheDataset(
                         data=val_list,
                         transform=val_transforms,
-                        cache_rate=1,
+                        cache_rate=0,
                         )
 
     # Define train and val DataLoader
